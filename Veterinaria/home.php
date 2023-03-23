9d82308a96576816d58fc8a2224b8002def23c04
Cata267 <124403068+Cata267@users.noreply.github.com>
2023-03-23 14:25:21 -0500

    Add files via upload

---
+++ Veterinaria/home.php
@@ -0,0 +1,7 @@
+
+Nombre del dueño:   <?php echo htmlspecialchars($_POST['nombredueño']);?>
+     Nombre de la mascota:   <?php echo htmlspecialchars($_POST['nombremascota']);?>
+     Edad de la mascota:   <?php echo (int)$_POST['edad'];?>
+     Telefono:    <?php echo htmlspecialchars($_POST['tel']);?>
+     Tipo de mascota:   <?php echo htmlspecialchars($_POST['tipo']);?>
+     Raza de la mascota:   <?php echo htmlspecialchars($_POST['raza']);?>
